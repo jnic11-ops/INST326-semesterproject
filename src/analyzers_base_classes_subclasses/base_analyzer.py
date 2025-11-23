9cb737a8fdda107e25d4965e44e5ca1498d1c057
jnic11-ops <jnic11@terpmail.umd.edu>
2025-11-23 18:09:56 -0500

    Update base_analyzer.py

--- src/analyzers_base_classes_subclasses/base_analyzer.py
+++ src/analyzers_base_classes_subclasses/base_analyzer.py
@@ -2,7 +2,10 @@
 from abc import ABC, abstractmethod
 
 class BaseAnalyzer(ABC):
-    """Abstract base for all analyzers."""
+    """ Abstract base for all analyzers."""
+
+    def __init__(self, data_manager):
+        self.data_manager = data_manager
 
     @abstractmethod
     def analyze(self):
